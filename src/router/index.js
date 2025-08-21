25e8692bb2e80247871b9834a4160609bd4064da
Yangshengzhou <3555844679@qq.com>
2025-08-21 20:48:41 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import AppLayout from '../views/AppLayout.vue'
 import HomeView from '../views/HomeView.vue'
 import AutoInfoView from '../views/AutoInfoView.vue'
 import AITakeoverView from '../views/AITakeoverView.vue'
-import DataExportView from '../views/DataExportView.vue'
+import OtherBoxView from '../views/OtherBoxView.vue'
 
 const routes = [
   {
@@ -13,8 +13,7 @@ const routes = [
       { path: '', name: 'home', component: HomeView },
       { path: 'auto_info', name: 'auto_info', component: AutoInfoView },
       { path: 'ai_takeover', name: 'ai_takeover', component: AITakeoverView },
-      { path: 'data_export', name: 'data_export', component: DataExportView },
-      { path: 'data_export', name: 'data_export', component: DataExportView },
+      { path: 'other_box', name: 'other_box', component: OtherBoxView },
     ]
   }
 ]
